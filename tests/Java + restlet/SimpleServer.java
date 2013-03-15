2e3bf4eeae36e7e4d4f4af44c2345218823a4408
Dicebot <m.strashun@gmail.com>
2013-03-15 22:37:27 +0200

    Java version source + restlet jar. Not yet working.

---
+++ tests/Java + restlet/SimpleServer.java
@@ -0,0 +1,18 @@
+import org.restlet.Application;
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+ 
+ public class SimpleServer extends Application {
+     public static void main(String[] args) throws Exception {
+         new Server(Protocol.HTTP, 8111, GreetingResource.class).start();
+     }
+
+     public static class GreetingResource extends ServerResource {
+         @Get
+         public String getHello() {
+             return "Hello, " + getQueryValue("name");
+         }
+     }
+ }
